b87a8876ee36400bf05c5c77c2dd088c1a30ae20
Lucas Martiniano <l231036@dac.unicamp.br>
2022-05-09 14:09:06 -0300

    added: .npz saving

---
+++ README.md
@@ -0,0 +1,24 @@
+# Blackhole 2d 
+
+A toy implementation of ~~neural network~~ normal chess written while livestreaming.
+
+A toy simulation of a ~kinda~ 2d blackhole
+![](screenshot.png)
+
+Nice TODO
+-----
+* Add relativistic effects (currently using newtonian physics :/)
+
+Usage
+-----
+
+```
+ pip3 install numpy matplotlib tqdm
+ # then...
+ ./to_image.py   
+ # set flag RE_CALCULATE=True inside <to_image.py> for new images every time
+```
+
+Implementation
+-----
+Check https://www.asc.ohio-state.edu/orban.14/stemcoding/blackhole_derivation_slide3.png for more information
--- to_image.py
+++ to_image.py
@@ -1,12 +1,16 @@
 import blackhole
+import numpy as np
+from tqdm import tqdm
+
+RE_CALCULATE = False
 
 def timer(f):
-  from time import time
+  from time import perf_counter
   def wrapper(*args, **kwargs):
-    t0 = time()
+    t0 = perf_counter()
     val = f(*args, **kwargs)
-    t1 = time()
-    print(f'{f} em: {t1-t0}')
+    t1 = perf_counter()
+    print(f'function <{f.__name__}> : {t1-t0}')
     return val
   return wrapper
 
@@ -14,15 +18,15 @@ def timer(f):
 def calculate():
   m87 = blackhole.Blackhole()
 
-  cluster = [blackhole.Matter(m87) for _ in range(40)]
-  for matter in cluster:
-    for _ in range(1000):
+  cluster = [blackhole.Matter(m87) for _ in range(1000)]
+  for matter in tqdm(cluster):
+    for _ in range(900):
       if not matter.update():
         break
 
-  beam = [blackhole.Photon(m87) for _ in range(40)]
-  for photon in beam:
-    for _ in range(1000):
+  beam = [blackhole.Photon(m87) for _ in range(80)]
+  for photon in tqdm(beam):
+    for _ in range(850):
       if not photon.update():
         break
   return cluster, beam
@@ -36,29 +40,41 @@ def translate(value, a1, b1, a2, b2):
   return int(round(newvalue, 3))
 
 @timer
-def normalizeRange(cluster, beam, img_size=1000):
-  YELLOW = 250
-  GREY = 100
+def normalizeRange(cluster, beam, img_size=1500):
+  MATTER_CLR = 0.7
+  PHOTON_CLR = 1
   
   import numpy as np
   img = np.zeros((img_size,img_size))
 
-  for matter in cluster:
+  for matter in tqdm(cluster):
     for x,y in zip(matter.x_coords, matter.y_coords):
       norm_x = translate(x, -5,5, 0, img_size)
       norm_y = translate(y, -5,5, 0, img_size)
-      img[norm_x, norm_y] = GREY
+      img[norm_x, norm_y] = MATTER_CLR
 
-  for photon in beam:
+  for photon in tqdm(beam):
     for x,y in zip(photon.x_coords, photon.y_coords):
       norm_x = translate(x, -5,5, 0, img_size)
       norm_y = translate(y, -5,5, 0, img_size)
-      img[norm_x, norm_y] = YELLOW
+      img[norm_x, norm_y] = PHOTON_CLR
+  return img
+
+def createNpz():
+  cluster, beam = calculate()
+  img = normalizeRange(cluster, beam)
+  np.savez('processed_img.npz', img)
+  print('Created file: processed_img.npz')
+
+def loadNpz():
+  img = np.load('processed_img.npz')['arr_0']
+  print('Loaded file: <processed_img.npz>')
   return img
 
 if __name__ == '__main__':
-  cluster, beam = calculate()
-  img = normalizeRange(cluster, beam)
   import matplotlib.pyplot as plt
+  if RE_CALCULATE:
+    createNpz()
+  img = loadNpz()
   plt.imshow(img, cmap='hot')
   plt.show()
